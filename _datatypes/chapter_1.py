4949eaecd4c24e4137f7519e7d6f65531f9d676d
Sahil Singh <kswayam2000@gmail.com>
2025-12-05 13:14:58 +0530

    mutable and immutable

--- _datatypes/chapter_1.py
+++ _datatypes/chapter_1.py
@@ -2,3 +2,8 @@ sugar_amount = 3
 print(f"initial sugar : {sugar_amount}")
 
 # example of immutable
+
+sugar_amount = 12 
+print(f"initial sugar : {sugar_amount}")
+
+# in memory both (2) and (12) saved but in print sugar_amount , only refernce change. since no change in memory , it is immutble 
--- _datatypes/chapter_2.py
+++ _datatypes/chapter_2.py
@@ -1,12 +1,15 @@
 # example of mutable
-spice_mix = set()
+spice_mix = set()  
 
-print(f"initial spice mix : {spice_mix}")
+# initially empty
+print(f"initial spice mix : {spice_mix}") 
 print(f"initial spice mix : {id(spice_mix)}") 
 
+# adding spices
 spice_mix.add("ginger") 
 spice_mix.add("turmeric") 
 spice_mix.add("garlic")
 
+# now can add so it mutaded spice_mix in memory 
 print(f"initial spice mix : {spice_mix}")
 print(f"initial spice mix : {id(spice_mix)}")

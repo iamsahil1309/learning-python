8acef681756581732893a3f44d502631656e5903
Sahil Singh <kswayam2000@gmail.com>
2025-12-05 13:51:53 +0530

    booleans

---
+++ _datatypes/chapter_4.py
@@ -0,0 +1,7 @@
+# boolean
+
+is_boiling = True  # use first capital letter and consider as 1 and , false is 0
+str_count = 5
+total_count = str_count + is_boiling # upcasting (when true false value consider as 1 and 0)
+
+print(f"{total_count}")

2e1f744c31b707c87f0a3541a1e5492ed52a48b3
Sahil Singh <kswayam2000@gmail.com>
2025-12-05 13:37:19 +0530

    integers

---
+++ _datatypes/chapter_3.py
@@ -0,0 +1,21 @@
+# INTEGER
+
+black_tea = 14
+ginger_tea = 4
+
+total_tea = black_tea + ginger_tea
+print(f"total tea is {total_tea}")
+
+# division and division with no decimal
+
+milk_litres = 14
+servings = 4
+
+# 3.5
+milk_per_Serving = milk_litres / servings
+print(f"{milk_per_Serving}")
+
+# 3
+milk_per_Serving = milk_litres // servings
+print(f"{milk_per_Serving}")
+

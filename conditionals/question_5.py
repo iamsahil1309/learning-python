6aa64be25e4413712d3cf590438a0a58db573ff4
Sahil Singh <kswayam2000@gmail.com>
2025-12-09 12:44:10 +0530

    question 5

---
+++ conditionals/question_5.py
@@ -0,0 +1,12 @@
+# Suggest an activity based on the weather (e.g., Sunny - Go for a walk, Rainy - Read a book, Snowy - Build a snowman).
+
+weather = 'Sunny'
+
+if weather == 'Sunny' :
+    actiivity = 'go for a walk'
+elif weather == 'Rainy' :
+    actiivity = 'Read a book'
+elif weather == 'Snowy' :
+    actiivity = 'build a snowman'
+
+print(actiivity)

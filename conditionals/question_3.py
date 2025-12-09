ef7c675e0493a800991ddd808b9f94b67648213e
Sahil Singh <kswayam2000@gmail.com>
2025-12-09 12:35:16 +0530

    question 3

---
+++ conditionals/question_3.py
@@ -0,0 +1,18 @@
+# Assign a letter grade based on a student's score: A (90-100), B (80-89), C (70-79), D (60-69), F (below 60).
+
+score = int(input("enter your score : "))
+
+if score >= 101 : 
+    print("please verify your score again.")
+    exit()
+
+if score >= 90 :
+    print("A")
+elif score >= 80 : 
+    print("B")
+elif score >= 70 :
+    print("C")
+elif score >= 60 :
+    print("D")
+else :
+    print("F")    

fd16c451e59a6fa63dadcf25fd71a74b689801f3
Sahil Singh <kswayam2000@gmail.com>
2025-12-09 11:49:44 +0530

    solving conditonals question 1 in python

---
+++ conditionals/question_1.py
@@ -0,0 +1,15 @@
+# Clasify a person age group: child(<13), teenager(13-19), Adult(20-59),Senior(60+)
+
+your_age = input("enter your age : ")
+your_age_int = int(your_age)
+
+# print(f"{your_age_int}")
+
+if your_age_int < 13 : 
+    print("child")
+elif your_age_int < 20 : 
+    print("teenager")
+elif your_age_int < 60 :
+    print("adult")
+else : 
+    print("old")            

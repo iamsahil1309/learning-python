452505ebeb9126160b82c3bc2b635372739a37aa
Sahil Singh <kswayam2000@gmail.com>
2025-12-10 12:51:39 +0530

    loops in python

---
+++ loops/question.py
@@ -0,0 +1,4 @@
+# a tea stall owner has digital token display for every customer in line , a token is printed and chai is serverd 
+
+for token_number in range(1,11) :
+    print(f"Serving Chai to Token#{token_number}")

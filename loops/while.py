ffd3e90f4effc315dbdeb90b55228617cb344548
Sahil Singh <kswayam2000@gmail.com>
2025-12-13 12:54:50 +0530

    while loop in python

---
+++ loops/while.py
@@ -0,0 +1,9 @@
+# use while loop to print something until  the statement is false 
+
+temprature = 40
+
+while  temprature <=100 :
+    print(f"current temprature is {temprature}")
+    temprature += 15
+
+print("Tea is ready to boil")

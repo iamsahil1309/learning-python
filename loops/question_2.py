660da6240df36864864a8df75bb76324c228f258
Sahil Singh <kswayam2000@gmail.com>
2025-12-13 12:36:13 +0530

    n loop

---
+++ loops/question_2.py
@@ -0,0 +1,4 @@
+orders = ["sahil", "yugji", "jihyo", "goku" ]
+
+for name in orders : 
+    print(f"order ready for {name}")
--- weather app/app.js
+++ weather app/app.js
@@ -1,8 +1,21 @@
-let key = "d19fef121990e40610dd36d828cc9eac";
+let id = "d19fef121990e40610dd36d828cc9eac";
 
-async function weather(city) {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`)
-      const data = await response.json()
+let url =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&appid =" + id;
+
+const city = document.getElementById("city");
+const search = document.getElementById("searchBtn");
+
+search.addEventListener("click", () => {
+  if (city.value != "") {
+    searchWeather();
+  }
+});
+
+const searchWeather = () => {
+  fetch(url + "&q=" + city.value)
+    .then((res) => res.json())
+    .then((data) => {
       console.log(data)
-}
+    });
+};

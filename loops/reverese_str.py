09f044d6dd72904cb702757e28cc7bcd8ea75390
Sahil Singh <kswayam2000@gmail.com>
2025-12-14 13:47:47 +0530

    loops q4

---
+++ loops/reverese_str.py
@@ -0,0 +1,8 @@
+# reverse a string using a loop 
+
+input_str = input("enter the string value : ")
+reverse_str = ""
+
+for char in input_str :
+    reverse_str = input_str[::-1]
+print(f"{reverse_str}")

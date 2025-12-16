c28542bfdebd5dc656c0a192549fade56f29ef2a
Sahil Singh <kswayam2000@gmail.com>
2025-12-16 14:00:14 +0530

    function 2

---
+++ _functionss/fun_2.py
@@ -0,0 +1,21 @@
+def fetch_sales() :
+    print("Sales data fetched")
+
+def filter_valid_orders() :
+    print("orders are filtered")
+
+
+def summarize_data() :
+    print("data is summarized")
+    
+
+def generate_report() :
+    fetch_sales()
+    filter_valid_orders()
+    summarize_data()
+    print("report is ready")
+
+
+
+generate_report()
+

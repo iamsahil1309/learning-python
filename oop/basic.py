aff0591a66a8072ba3e465431dc7fc541d50a3fd
Sahil Singh <kswayam2000@gmail.com>
2025-12-29 19:11:45 +0530

    comit

--- oop/basic.py
+++ oop/basic.py
@@ -1,11 +1,14 @@
 # create a car class with attributes like brand and model then create instance of this class 
 class Car : 
     def __init__(self, brand, model):
-        self.brand = brand
+        self.__brand = brand
         self.model = model
 
+    def get_brand(self) :
+        return self.__brand
+
     def full_name(self) :
-        return f"{self.brand} {self.model}"
+        return f"{self.__brand} {self.model}"
 
 class ElectricCar(Car) :
     def __init__(self, brand, model, battery_size):
@@ -15,12 +18,10 @@ class ElectricCar(Car) :
 
 # INHERITANCE
 my_tesla = ElectricCar("Tesla","Model S", "85kwh")
-print(my_tesla.brand) 
+print(my_tesla.get_brand) 
 print(my_tesla.model) 
 print(my_tesla.battery_size) 
 
 print(my_tesla.full_name())  #can access the method or function present in its parent class
-# my_car = Car("toyota","supra")
-# print(my_car.brand)
 # print(my_car.full_name())
 

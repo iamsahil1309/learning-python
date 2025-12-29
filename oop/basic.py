87927f9f40613bed293acb77962142459f142f7d
Sahil Singh <kswayam2000@gmail.com>
2025-12-29 19:11:32 +0530

    encapsulation and getting the method

--- oop/basic.py
+++ oop/basic.py
@@ -13,12 +13,13 @@ class ElectricCar(Car) :
         self.battery_size = battery_size
         
 
+# INHERITANCE
 my_tesla = ElectricCar("Tesla","Model S", "85kwh")
 print(my_tesla.brand) 
 print(my_tesla.model) 
 print(my_tesla.battery_size) 
 
-print(my_tesla.full_name())
+print(my_tesla.full_name())  #can access the method or function present in its parent class
 # my_car = Car("toyota","supra")
 # print(my_car.brand)
 # print(my_car.full_name())

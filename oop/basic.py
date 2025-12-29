04fc8e4af1872ead97cbf4d96c3d733f4a455eaf
Sahil Singh <kswayam2000@gmail.com>
2025-12-29 14:00:22 +0530

    inheritance and super

--- oop/basic.py
+++ oop/basic.py
@@ -4,6 +4,22 @@ class Car :
         self.brand = brand
         self.model = model
 
-my_car = Car("toyota","supra")
-print(my_car.brand)
+    def full_name(self) :
+        return f"{self.brand} {self.model}"
+
+class ElectricCar(Car) :
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+        
+
+my_tesla = ElectricCar("Tesla","Model S", "85kwh")
+print(my_tesla.brand) 
+print(my_tesla.model) 
+print(my_tesla.battery_size) 
+
+print(my_tesla.full_name())
+# my_car = Car("toyota","supra")
+# print(my_car.brand)
+# print(my_car.full_name())
 

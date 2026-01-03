54c7670303e1376f9960a2b7e3a89d8d45ed7d7f
Sahil Singh <kswayam2000@gmail.com>
2026-01-03 12:42:43 +0530

    inherit other class

--- oop/basic.py
+++ oop/basic.py
@@ -10,3 +10,13 @@ my_car = Car("toyota", "corolla")
 # print(my_car.brand)
 # print(my_car.model)
 print(my_car.full_name())
+
+# INHERITANCE
+class ElectricCar(Car) :
+    def __init__(self, brand, model, battery_size):
+        self.battery_size = battery_size
+        super().__init__(brand, model)
+
+my_electric_car = ElectricCar("Tesla", "Xs", "85kwh")
+print(my_electric_car.full_name())
+print(my_electric_car.brand)

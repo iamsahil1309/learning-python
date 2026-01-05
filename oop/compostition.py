107273b78ee86dc47c0e14448987f4e0f51c2059
Sahil Singh <kswayam2000@gmail.com>
2026-01-05 12:30:51 +0530

    inheritance and composition

---
+++ oop/compostition.py
@@ -0,0 +1,32 @@
+class BaseChai :
+    def __init__(self, type_):
+        self.type = type_
+
+    def prepare (self) :
+        print(f"{self.type}")
+
+#INHERITANCE
+class MasalaChai(BaseChai) :
+    def add_spice(self) :
+        print("ginger")
+
+#COMPOSITION
+class ChaiShop :
+    chai_cls = BaseChai
+
+    def __init__(self):
+        self.chai = self.chai_cls("Regular")
+
+    def serve(self) :
+        print(f"serving {self.chai.type} chai")
+        self.chai.prepare()
+
+class FancyShop(ChaiShop) :
+    chai_cls = MasalaChai
+
+shop = ChaiShop()
+fancy = FancyShop()
+
+shop.serve()
+fancy.serve()
+fancy.chai.add_spice()

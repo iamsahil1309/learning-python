e40c991f17207de149c208115e99ab40386c0d14
Sahil Singh <kswayam2000@gmail.com>
2026-01-09 12:06:31 +0530

    error example 2

--- error_handeling/try.py
+++ error_handeling/try.py
@@ -7,3 +7,17 @@ except KeyError :
     print("The key you are trying to access doesnt exist!")
 
 print("hello") 
+
+def process_order(item, quantity) :
+    try :
+        price = {"masala" : 40}[item]
+        cost = price * quantity
+        print(f"total cost is {cost}")
+    except KeyError :
+        print("Sorry that chai is not in the menu")
+    except TypeError :
+        print("Quantity must be in number")
+
+process_order("ginger", 4)
+process_order("masala", "two")     #operator overloading
+process_order("masala", 4)

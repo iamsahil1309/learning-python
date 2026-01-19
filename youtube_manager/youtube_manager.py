df7233febb36d14e314c1fa1977538efe94fb6d7
Sahil Singh <kswayam2000@gmail.com>
2026-01-19 21:35:48 +0530

    youtube manager python

---
+++ youtube_manager/youtube_manager.py
@@ -0,0 +1,28 @@
+
+def load_data(): 
+    pass
+
+def main():
+    videos = load_data()
+    while True: 
+        print("\n Youtube Manager | Choose an option ")
+        print("1. List all Youtube Videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = input("Enter your choice: ")
+
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                break
+            case _:
+                print("Invalid Choice")

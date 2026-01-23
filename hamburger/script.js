22ed20ecf729157cefee1e97395b3b20a810833b
Sahil Singh <kswayam2000@gmail.com>
2026-01-23 14:59:28 +0530

    starting pydantic

--- hamburger/script.js
+++
@@ -1,7 +0,0 @@
-const burger = document.getElementById("burger")
-const nav = document.querySelector("nav")
-
-burger.addEventListener("click", () => {
-  nav.style.transform = "translateX(0px)"
-})
-

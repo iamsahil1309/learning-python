f021662ac507cf282c9d85935fd3cea9d5fbfacd
Sahil Singh <kswayam2000@gmail.com>
2026-03-10 13:33:43 +0530

    lists

---
+++ _datatypes/chapter_5.py
@@ -0,0 +1,11 @@
+# lists
+spice_mix = ["ginger", "clove", "cardmam" ]
+
+print(f"{spice_mix}")
+
+spice_mix.append("tea")
+print(f"{spice_mix}")
+
+counting = spice_mix.count
+print(f"{counting}")
+

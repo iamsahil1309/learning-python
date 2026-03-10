35e41edbd4a95d8e8a7cb3d6da6bc6a245ad901c
Sahil Singh <kswayam2000@gmail.com>
2026-03-10 11:53:14 +0530

    changes

--- _datatypes/chapter_4.py
+++
@@ -1,7 +0,0 @@
-# boolean
-
-is_boiling = True  # use first capital letter and consider as 1 and , false is 0
-str_count = 5
-total_count = str_count + is_boiling # upcasting (when true false value consider as 1 and 0)
-
-print(f"{total_count}")
